8b7149e4f47aa697f8249d1f5b668a67bb0a8689
shurona <khc4669@naver.com>
2025-05-14 20:50:05 +0900

    feat: user쪽 추가

---
+++ src/main/java/com/shrona/line_demo/user/domain/User.java
@@ -0,0 +1,51 @@
+package com.shrona.line_demo.user.domain;
+
+
+import com.shrona.line_demo.common.entity.BaseEntity;
+import com.shrona.line_demo.line.domain.LineUser;
+import com.shrona.line_demo.user.domain.vo.PhoneNumber;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "_user")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Embedded
+    @Column(name = "phone_number", unique = true)
+    private PhoneNumber phoneNumber;
+
+    @Column(name = "line_id", unique = true)
+    private String lineId;
+
+    @Column
+    private String description;
+
+    @OneToMany(mappedBy = "user")
+    private List<UserGroup> userGroupList = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "line_user_id")
+    private LineUser lineUser;
+
+}
---
+++ src/main/java/com/shrona/line_demo/user/domain/UserGroup.java
@@ -0,0 +1,33 @@
+package com.shrona.line_demo.user.domain;
+
+import com.shrona.line_demo.common.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_group")
+public class UserGroup extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+}
