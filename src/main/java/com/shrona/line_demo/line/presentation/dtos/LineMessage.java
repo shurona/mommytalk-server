0f4c193b76aaf78a122e7203f73879061e72c409
shurona <khc4669@naver.com>
2025-05-15 11:30:33 +0900

    feat: Json을 처리할 수 있도록 추가

--- src/main/java/com/shrona/line_demo/line/presentation/dtos/LineMessage.java
+++ src/main/java/com/shrona/line_demo/line/presentation/dtos/LineMessage.java
@@ -1,11 +1,14 @@
 package com.shrona.line_demo.line.presentation.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record LineMessage(
     @JsonProperty("type") String type,
     @JsonProperty("id") String id,
-    @JsonProperty("text") String text
+    @JsonProperty("text") String text,
+    @JsonProperty("quoteToken") String quoteToken
 ) {
 
 }
