9037592fcdc60190b3075a5fdc3ae8c1e69b0fad
shurona <khc4669@naver.com>
2025-05-16 21:20:15 +0900

    feat: PhoneNumber내부 비교 로직 및 생성 로직 추가

--- src/main/java/com/shrona/line_demo/user/domain/vo/PhoneNumber.java
+++ src/main/java/com/shrona/line_demo/user/domain/vo/PhoneNumber.java
@@ -2,6 +2,7 @@ package com.shrona.line_demo.user.domain.vo;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import java.util.Objects;
 import java.util.regex.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,7 +15,6 @@ import org.springframework.util.StringUtils;
 public class PhoneNumber {
 
     public final static String PHONE_NUMBER_PATTERN = "^\\d{3}-\\d{3,4}-\\d{4}$";
-
     @Column(name = "phone_number", unique = true, nullable = true)
     private String phoneNumber;
 
@@ -32,6 +32,41 @@ public class PhoneNumber {
         }
 
         this.phoneNumber = phoneNumber;
+    }
+
+    public static PhoneNumber changeWithoutError(String phoneNumber) {
+        // Check Valid PhoneNumber
+        if (!StringUtils.hasText(phoneNumber) || !Pattern.matches(PHONE_NUMBER_PATTERN,
+            phoneNumber)) {
+            return null;
+        }
+
+        return new PhoneNumber(phoneNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PhoneNumber that = (PhoneNumber) o;
+        return Objects.equals(getPhoneNumber(), that.getPhoneNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPhoneNumber());
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneNumber{" +
+            "phoneNumber='" + phoneNumber + '\'' +
+            '}';
     }
 
 }
