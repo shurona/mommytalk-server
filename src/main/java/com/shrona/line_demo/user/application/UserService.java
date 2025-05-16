a3f7d60dc4a6d2a3f77285402f2a9503eb6005a7
shurona <khc4669@naver.com>
2025-05-16 21:19:34 +0900

    feat: User Service

--- src/main/java/com/shrona/line_demo/user/application/UserService.java
+++ src/main/java/com/shrona/line_demo/user/application/UserService.java
@@ -1,5 +1,29 @@
 package com.shrona.line_demo.user.application;
 
+import com.shrona.line_demo.user.domain.User;
+import java.util.List;
+
 public interface UserService {
+
+
+    /**
+     * 유저 생성
+     */
+    public User createUser(String phoneNumber);
+
+    /**
+     * 유저 단일 조회
+     */
+    public User findUserByPhoneNumber(String phoneNumber);
+
+    /**
+     * 유저 목록 조회
+     */
+    public List<User> findUserList();
+
+    /**
+     * 휴대전화 번호 입력을 기준으로 없는 유저는 생성 후 조회
+     */
+    public List<User> findOrCreateUsersByPhoneNumbers(List<String> phoneNumberList);
 
 }
--- src/main/java/com/shrona/line_demo/user/application/UserServiceImpl.java
+++ src/main/java/com/shrona/line_demo/user/application/UserServiceImpl.java
@@ -1,5 +1,132 @@
 package com.shrona.line_demo.user.application;
 
+import com.shrona.line_demo.common.core.PhoneProcess;
+import com.shrona.line_demo.line.domain.LineUser;
+import com.shrona.line_demo.line.infrastructure.LineUserJpaRepository;
+import com.shrona.line_demo.user.common.utils.UserUtils;
+import com.shrona.line_demo.user.domain.User;
+import com.shrona.line_demo.user.domain.vo.PhoneNumber;
+import com.shrona.line_demo.user.infrastructure.UserJpaRepository;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
 public class UserServiceImpl implements UserService {
 
+    // jpa
+    private final UserJpaRepository userRepository;
+    private final LineUserJpaRepository lineUserRepository;
+
+    // 휴대폰 관련 process 처리
+    private final PhoneProcess phoneProcess;
+    private final UserUtils userUtils;
+
+    @Transactional
+    public User createUser(String phoneNumber) {
+
+        PhoneNumber number = new PhoneNumber(phoneNumber);
+
+        Optional<User> userInfo = userRepository.findByPhoneNumber(
+            number);
+
+        // 있으면 이미 있는 유저를 전달한다.
+        if (userInfo.isPresent()) {
+            return userInfo.get();
+        }
+
+        User user = User.createUser(number);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findUserByPhoneNumber(String phoneNumber) {
+        PhoneNumber number = new PhoneNumber(phoneNumber);
+        Optional<User> userInfo = userRepository.findByPhoneNumber(number);
+
+        return userInfo.orElse(null);
+    }
+
+    @Override
+    public List<User> findUserList() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public List<User> findOrCreateUsersByPhoneNumbers(List<String> phoneNumberList) {
+
+        // 전화번호 변환 및 필터링
+        List<PhoneNumber> userPhoneList = phoneProcess.validateAndConvertPhoneNumbers(
+            phoneNumberList);
+
+        // 기존 사용자 조회
+        List<User> existingUsers = userRepository.findByPhoneNumberIn(userPhoneList);
+
+        // 기존 사용자들의 전화번호를 추출한다.
+        Set<PhoneNumber> existingPhones = userUtils.extractPhoneNumbers(existingUsers);
+
+        // 입력받은 휴대전화 목록 중에서 등록되지 않은 전화 번호 목록을 추출
+        List<PhoneNumber> notFoundNumbers = findNotFoundPhoneNumbers(userPhoneList, existingPhones);
+
+        // 등록되지 않은 휴대전화 번호 목록으로 lineUser 목록을 조회한다.
+        Map<PhoneNumber, LineUser> phoneToLineUser = createPhoneToLineUserMap(notFoundNumbers);
+
+        // 새로운 유저 목록을 라인이 있는 유저와 없는 유저를 구분해서 생성해준다.
+        List<User> newUsers = userRepository.saveAll(
+            createNewUsers(notFoundNumbers, phoneToLineUser)
+        );
+
+        // 두 List를 합쳐서 반환
+        return combineUsers(newUsers, existingUsers);
+    }
+
+
+    private List<User> combineUsers(List<User> existing, List<User> newUsers) {
+        return Stream.concat(existing.stream(), newUsers.stream())
+            .toList();
+    }
+
+
+    private List<PhoneNumber> findNotFoundPhoneNumbers(
+        List<PhoneNumber> userPhoneList,
+        Set<PhoneNumber> foundPhoneNumbers
+    ) {
+        return userPhoneList.stream()
+            .filter(phone -> !foundPhoneNumbers.contains(phone))
+            .toList();
+    }
+
+    private Map<PhoneNumber, LineUser> createPhoneToLineUserMap(List<PhoneNumber> notFoundNumbers) {
+        List<LineUser> lineUsers = lineUserRepository.findByPhoneNumberIn(notFoundNumbers);
+        return lineUsers.stream()
+            .collect(Collectors.toMap(
+                LineUser::getPhoneNumber,
+                Function.identity()
+            ));
+    }
+
+    /*
+        라인이 있는 유저와 없는 유저와 구분해서 User 객체 생성
+     */
+    private List<User> createNewUsers(
+        List<PhoneNumber> notFoundNumbers,
+        Map<PhoneNumber, LineUser> phoneToLineUser
+    ) {
+        return notFoundNumbers.stream()
+            .map(phone -> phoneToLineUser.containsKey(phone)
+                ? User.createUserWithLine(phone, phoneToLineUser.get(phone))
+                : User.createUser(phone))
+            .toList();
+    }
 }
