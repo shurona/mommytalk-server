ee6627e05ebb45ce67e992d3a4b29492fd3d326e
shurona <khc4669@naver.com>
2025-05-18 14:55:14 +0900

    feat: 예약 메시지 조회

--- src/main/java/com/shrona/line_demo/line/infrastructure/MessageLogJpaRepository.java
+++ src/main/java/com/shrona/line_demo/line/infrastructure/MessageLogJpaRepository.java
@@ -1,8 +1,13 @@
 package com.shrona.line_demo.line.infrastructure;
 
 import com.shrona.line_demo.line.domain.MessageLog;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MessageLogJpaRepository extends JpaRepository<MessageLog, Long> {
+
+    @Query("select log from MessageLog log where log.reserveTime <= now() and status = 'PREPARE'")
+    List<MessageLog> findAllByReservedMessage();
 
 }
