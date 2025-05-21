6046d34c7bd7d4ab81b255ea7c668e31c54df21d
shurona <khc4669@naver.com>
2025-05-21 23:05:11 +0900

    test: Line Service 테스트

---
+++ src/test/java/com/shrona/line_demo/user/infrastructure/UserJpaRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.shrona.line_demo.user.infrastructure;
+
+import com.shrona.line_demo.line.domain.LineUser;
+import com.shrona.line_demo.line.infrastructure.LineUserJpaRepository;
+import com.shrona.line_demo.user.domain.User;
+import com.shrona.line_demo.user.domain.vo.PhoneNumber;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class UserJpaRepositoryTest {
+
+    @Autowired
+    private UserJpaRepository userJpaRepository;
+
+    @Autowired
+    private LineUserJpaRepository lineUserJpaRepository;
+
+    @Test
+    public void 번호가_있지만_라인이_빈_경우_조회() {
+
+        // given
+        PhoneNumber number = new PhoneNumber("010-3393-1234");
+        LineUser lineUser = lineUserJpaRepository.save(LineUser.createLineUser("lineOne"));
+
+        User user = userJpaRepository.save(User.createUser(number));
+
+        // when
+        Optional<User> userInfo = userJpaRepository.findByPhoneNumberAndLineUserIsNull(
+            number);
+
+        // then
+        Assertions.assertThat(userInfo.isPresent()).isTrue();
+    }
+
+    @Test
+    public void 번호가_있지만_라인이_이미저장되어_있는_경우() {
+
+        // given
+        PhoneNumber number = new PhoneNumber("010-3393-1234");
+        LineUser lineUser = lineUserJpaRepository.save(LineUser.createLineUser("lineOne"));
+
+        User user = userJpaRepository.save(User.createUserWithLine(number, lineUser));
+
+        // when
+        Optional<User> userInfo = userJpaRepository.findByPhoneNumberAndLineUserIsNull(
+            number);
+
+        // then
+        Assertions.assertThat(userInfo.isPresent()).isFalse();
+    }
+
+}
