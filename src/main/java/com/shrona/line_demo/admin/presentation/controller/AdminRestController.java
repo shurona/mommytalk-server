6b894710631d3480e1a7ea4644912171d843f9d0
shurona <khc4669@naver.com>
2025-05-22 21:22:48 +0900

    feat: restcontroller 이름 변경

--- src/main/java/com/shrona/line_demo/admin/presentation/controller/AdminRestController.java
+++ src/main/java/com/shrona/line_demo/admin/presentation/controller/AdminRestController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin")
 @RestController
-public class AdminController {
+public class AdminRestController {
 
     private final AdminService adminService;
 
