5411ec627e42ccf58e95367514e02ffb6d8ea42a
shurona <khc4669@naver.com>
2025-05-23 14:29:40 +0900

    feat: MessageService comment 추가

--- src/main/java/com/shrona/line_demo/line/application/MessageService.java
+++ src/main/java/com/shrona/line_demo/line/application/MessageService.java
@@ -9,15 +9,30 @@ import org.springframework.data.domain.Pageable;
 
 public interface MessageService {
 
+    /**
+     * 메시지 타입 생성
+     */
     MessageType createMessageType(String title, String text);
 
+    /**
+     * 메시지 생성(발송 시 사용)
+     */
     List<MessageLog> createMessage
-        (Long messageTypeId, List<Long> groupId, LocalDateTime reserveTime, String content);
+    (Long messageTypeId, List<Long> groupId, LocalDateTime reserveTime, String content);
 
+    /**
+     * 메시지 단일 조회
+     */
     MessageLog findByMessageId(Long id);
 
+    /**
+     * 메시지 로그 조회
+     */
     Page<MessageLog> findMessageLogList(Pageable pageable);
 
+    /**
+     * 예약 된 메시지 목록 조회
+     */
     List<MessageLog> findReservedMessage();
-    
+
 }
