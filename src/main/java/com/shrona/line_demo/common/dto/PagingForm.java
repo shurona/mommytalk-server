216a5f6bbbb19245b1a8fe882db9e109d94e35e8
shurona <khc4669@naver.com>
2025-05-25 17:48:44 +0900

    feat: 페이지네이션 이상한 부분 제거

--- src/main/java/com/shrona/line_demo/common/dto/PagingForm.java
+++ src/main/java/com/shrona/line_demo/common/dto/PagingForm.java
@@ -10,12 +10,13 @@ public record PagingForm(
 ) {
 
     public static PagingForm of(int pageNumber, int totalPages, String requestUrl) {
+        totalPages = (totalPages == 0) ? 1 : totalPages;
         // Front에서 페이지는 1부터 사작하므로 수정해준다.
         return new PagingForm(
             pageNumber,
             totalPages,
             pageNumber != 0,
-            (pageNumber + 1) != totalPages,
+            (pageNumber + 1) != totalPages, // 아무 페이지도 없거나 최종페이지 확인
             requestUrl);
     }
 
