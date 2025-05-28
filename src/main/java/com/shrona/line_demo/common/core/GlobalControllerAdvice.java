e50058dc5494668ebd21b53352183555f9f3d7d8
shurona <khc4669@naver.com>
2025-05-28 16:07:17 +0900

    feat: 주석 추가

--- src/main/java/com/shrona/line_demo/common/core/GlobalControllerAdvice.java
+++ src/main/java/com/shrona/line_demo/common/core/GlobalControllerAdvice.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
+    /**
+     * model로 currentPath를 기본적으로 등록한다.
+     */
     @ModelAttribute("currentPath")
     public String currentPath(HttpServletRequest request) {
         return request.getRequestURI();
