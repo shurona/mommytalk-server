cbc8192c8db2e776c6f355ca359b53d480c7f86d
shurona <khc4669@naver.com>
2025-05-28 16:10:04 +0900

    feat: Scheduler Config 등록

--- src/main/java/com/shrona/line_demo/common/core/StaticVariable.java
+++ src/main/java/com/shrona/line_demo/common/core/StaticVariable.java
@@ -2,8 +2,15 @@ package com.shrona.line_demo.common.core;
 
 public class StaticVariable {
 
+    /**
+     * 로그인 세션
+     */
     public final static String LOGIN_USER = "login-session";
-
     public final static int LOGIN_SESSION_TIME = 30 * 60;
+
+    /**
+     * 쓰레드 스케쥴 최대 크기
+     */
+    public static final int POOL_SIZE = 2;
 
 }
