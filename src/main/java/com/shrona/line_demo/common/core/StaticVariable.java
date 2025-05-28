ea29a81b3bb83a68f0689d6dd57a43ff8ca7054d
shurona <khc4669@naver.com>
2025-05-28 21:28:31 +0900

    feat: Static variable 설정

--- src/main/java/com/shrona/line_demo/common/core/StaticVariable.java
+++ src/main/java/com/shrona/line_demo/common/core/StaticVariable.java
@@ -11,6 +11,11 @@ public class StaticVariable {
     /**
      * 쓰레드 스케쥴 최대 크기
      */
-    public static final int POOL_SIZE = 2;
+    public final static int POOL_SIZE = 2;
 
+
+    /**
+     * 스케쥴 등록할 때 최소 숫자
+     */
+    public final static int NO_DELAY = 0;
 }
