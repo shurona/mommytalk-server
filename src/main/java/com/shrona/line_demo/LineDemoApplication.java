f93f0f6788f602a3dacd0e6b08c582c155323a55
shurona <khc4669@naver.com>
2025-05-29 15:28:55 +0900

    feat: 서버의 기본 utc 설정

--- src/main/java/com/shrona/line_demo/LineDemoApplication.java
+++ src/main/java/com/shrona/line_demo/LineDemoApplication.java
@@ -1,13 +1,20 @@
 package com.shrona.line_demo;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LineDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LineDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LineDemoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
 }
