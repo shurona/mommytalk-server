bc5387a2d4dd8014e03fcdabae6cbd653d605ee0
shurona <khc4669@naver.com>
2025-06-01 15:46:54 +0900

    feat: 채널 변경에 맞게  Controller 및 form 변경

--- src/main/java/com/shrona/line_demo/line/presentation/form/LineUserForm.java
+++ src/main/java/com/shrona/line_demo/line/presentation/form/LineUserForm.java
@@ -1,19 +1,24 @@
 package com.shrona.line_demo.line.presentation.form;
 
-import com.shrona.line_demo.user.domain.vo.PhoneNumber;
+import com.shrona.line_demo.line.domain.LineUser;
 import java.time.LocalDateTime;
 
 public record LineUserForm(
+    Long id,
     String lineId,
     String phoneNumber,
     LocalDateTime joinDate
 ) {
 
-    public static LineUserForm of(String lineId, PhoneNumber phoneNumber, LocalDateTime joinDate) {
-        String phone = phoneNumber != null ? phoneNumber.getPhoneNumber() : "";
-        return new LineUserForm(lineId,
+    public static LineUserForm of(LineUser lineUser) {
+        String phone =
+            lineUser.getPhoneNumber() != null
+                ? lineUser.getPhoneNumber().getPhoneNumber() : "";
+        return new LineUserForm(
+            lineUser.getId(),
+            lineUser.getLineId(),
             phone,
-            joinDate.plusHours(9)); // TODO: 서버는 utc 사용하고 클라이언트에서 반영하도록 변경
+            lineUser.getCreatedAt().plusHours(9)); // TODO: 서버는 utc 사용하고 클라이언트에서 반영하도록 변경
     }
 
 }
--- src/main/java/com/shrona/line_demo/line/presentation/mvc/LineUserController.java
+++ src/main/java/com/shrona/line_demo/line/presentation/mvc/LineUserController.java
@@ -1,15 +1,27 @@
 package com.shrona.line_demo.line.presentation.mvc;
 
+import static com.shrona.line_demo.common.core.StaticVariable.HOME_VIEW;
+
+import com.shrona.line_demo.common.dto.ChannelForm;
 import com.shrona.line_demo.common.dto.PagingForm;
+import com.shrona.line_demo.line.application.ChannelService;
 import com.shrona.line_demo.line.application.LineService;
+import com.shrona.line_demo.line.domain.Channel;
+import com.shrona.line_demo.line.domain.ChannelLineUser;
 import com.shrona.line_demo.line.domain.LineUser;
+import com.shrona.line_demo.line.presentation.dtos.LineUserUpdatePhoneRequestDto;
 import com.shrona.line_demo.line.presentation.form.LineUserForm;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
@@ -17,25 +29,72 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LineUserController {
 
     private final LineService lineService;
+    private final ChannelService channelService;
 
-    @GetMapping("/admin/line/friends/list")
+    @GetMapping("/admin/channels/{channelId}/friends/list")
     public String lineFriendView(
+        @RequestParam(value = "query", required = false) String query,
+        @PathVariable("channelId") Long channelId,
         @RequestParam(value = "page", defaultValue = "0") int pageNumber,
         Model model
     ) {
-        String lineFriendListViewUrl = "/admin/line/friends/list";
+        String lineFriendListViewUrl = "/admin/channels/" + channelId + "/friends/list";
 
-        Page<LineUser> lineUserList = lineService.findLineUserList(PageRequest.of(pageNumber, 20));
+        Optional<Channel> channelInfo = channelService.findChannelById(channelId);
+        // 채널정보가 없는 경우 그냥 홈으로 보낸다.
+        if (channelInfo.isEmpty()) {
+            return HOME_VIEW;
+        }
 
+        // 라인 유저 아이디 목록을 갖고 온다.
+        Page<ChannelLineUser> lineUserList;
+        if (query != null) {
+            lineUserList = lineService.findChannelLineUserListByChannelAndQuery(
+                channelInfo.get(), query, PageRequest.of(pageNumber, 20));
+        } else {
+            lineUserList = lineService.findChannelLineUserListByChannel(
+                channelInfo.get(), PageRequest.of(pageNumber, 20));
+        }
+
+        // 모델을 등록하는 부분
         model.addAttribute("pagingInfo",
             PagingForm.of(
                 lineUserList.getNumber(), lineUserList.getTotalPages(),
                 lineFriendListViewUrl));
 
+        // 모델 등록하는 부분
         model.addAttribute("friends", lineUserList.toList()
-            .stream().map(line -> LineUserForm.of(line.getLineId(), line.getPhoneNumber(),
-                line.getCreatedAt())));
+            .stream().map(line -> LineUserForm.of(line.getLineUser())));
+
+        // 채널 정보를 등록해준다.
+        registerChannelToModel(channelInfo.get(), model);
 
         return "friend/list";
+    }
+
+    /**
+     * 라인 유저의 휴대전화 업데이트
+     */
+    @PostMapping("/admin/friends/{id}")
+    public ResponseEntity<?> updateLinePhoneNumber(
+        @PathVariable("id") Long lineUserId,
+        @RequestBody LineUserUpdatePhoneRequestDto requestDto
+    ) {
+
+        LineUser lineUser = lineService.updateLineUserPhoneNumber(lineUserId,
+            requestDto.phone());
+
+        if (lineUser == null) {
+            return ResponseEntity.badRequest().body("잘못된 입력입니다.");
+        }
+
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * 모델에 채널 정보 등록
+     */
+    private void registerChannelToModel(Channel channel, Model model) {
+        model.addAttribute("channelInfo", ChannelForm.of(channel));
     }
 }
