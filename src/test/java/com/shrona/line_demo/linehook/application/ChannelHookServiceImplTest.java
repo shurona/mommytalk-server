a0a521a846cbd20b5e266db74a7e32cca4b4707a
shurona <khc4669@naver.com>
2025-06-01 19:00:18 +0900

    test: 휴대전화 전송 로직 추가

--- src/test/java/com/shrona/line_demo/linehook/application/ChannelHookServiceImplTest.java
+++ src/test/java/com/shrona/line_demo/linehook/application/ChannelHookServiceImplTest.java
@@ -1,7 +1,16 @@
 package com.shrona.line_demo.linehook.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.shrona.line_demo.line.application.LineService;
+import com.shrona.line_demo.line.application.sender.MessageSender;
 import com.shrona.line_demo.line.domain.Channel;
 import com.shrona.line_demo.line.domain.ChannelLineUser;
 import com.shrona.line_demo.line.domain.LineUser;
@@ -11,10 +20,12 @@ import com.shrona.line_demo.line.infrastructure.LineUserJpaRepository;
 import com.shrona.line_demo.user.domain.User;
 import com.shrona.line_demo.user.domain.vo.PhoneNumber;
 import com.shrona.line_demo.user.infrastructure.UserJpaRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -36,6 +47,12 @@ class ChannelHookServiceImplTest {
     @Autowired
     private ChannelLineUserJpaRepository channelLineUserRepository;
 
+    @MockitoBean
+    private MessageSender messageSender;
+
+    @MockitoBean
+    private LineService lineService;
+
 
     private LineUser lineUserOne;
     private LineUser lineUserTwo;
@@ -43,11 +60,11 @@ class ChannelHookServiceImplTest {
     private ChannelLineUser channelLineUserTwo;
     private User userBlankLineUser;
     private User userWithLineUser;
-
+    private Channel channel;
 
     @BeforeEach
     public void beforeEach() {
-        Channel channel = Channel.createChannel("name", "");
+        channel = Channel.createChannel("name", "");
         channelJpaRepository.save(channel);
 
         lineUserOne = lineUserRepository.save(LineUser.createLineUser("lineOne"));
@@ -70,20 +87,69 @@ class ChannelHookServiceImplTest {
         // given
         assertThat(userBlankLineUser.getLineId()).isNull();
 
+        when(lineService.findLineUserByPhoneNumber(anyString())).thenReturn(
+            Optional.empty());
+
         // when
-        channelHookService.validatePhoneAndMatchUser("010-1234-1234", channelLineUserTwo);
+        boolean b = channelHookService.validatePhoneAndMatchUser("010-1234-1234",
+            channelLineUserTwo);
         // then
         assertThat(userBlankLineUser.getLineId()).isEqualTo(lineUserTwo.getLineId());
+        assertThat(b).isTrue();
     }
 
     @Test
     public void 휴대전화_입력시_이미있는_경우_미_변경() {
         // given
         assertThat(userWithLineUser.getLineId()).isEqualTo(lineUserOne.getLineId());
+        when(lineService.findLineUserByPhoneNumber(anyString())).thenReturn(
+            Optional.of(mock(LineUser.class)));
+
         // when
-        channelHookService.validatePhoneAndMatchUser("010-1234-1235", channelLineUserTwo);
+        boolean b = channelHookService.validatePhoneAndMatchUser("010-1234-1235",
+            channelLineUserTwo);
         // then
         assertThat(userWithLineUser.getLineId()).isEqualTo(lineUserOne.getLineId());
+        assertThat(b).isFalse();
+    }
+
+    @Test
+    public void 성공_휴대전화_입력_시_단일전송_로직_테스트() {
+        // given
+        doNothing().when(messageSender).sendSingleMessageWithContents(any(), any(), anyString());
+        when(lineService.findLineUserByLineId(anyString())).thenReturn(
+            Optional.of(mock(LineUser.class)));
+
+        String phoneNumber = "010-1234-1234";
+//        String wrongPhoneNumber = "010-12-1234";
+
+        // when
+        channelHookService.sendLineMessageAfterSuccess(channel.getId(), lineUserOne.getLineId(),
+            phoneNumber);
+
+        // then
+        verify(messageSender).sendSingleMessageWithContents(any(Channel.class), any(LineUser.class),
+            anyString());
+    }
+
+    @Test
+    public void 잘못된_휴대전화_입력_시_단일전송_로직_테스트() {
+        // given
+        doNothing().when(messageSender).sendSingleMessageWithContents(any(), any(), anyString());
+        when(lineService.findLineUserByLineId(anyString())).thenReturn(
+            Optional.of(mock(LineUser.class)));
+
+        String wrongPhoneNumber = "010-12-1234";
+
+        // when
+        channelHookService.sendLineMessageAfterSuccess(channel.getId(), lineUserOne.getLineId(),
+            wrongPhoneNumber);
+
+        // then
+        verify(messageSender, never())
+            .sendSingleMessageWithContents(any(Channel.class),
+                any(LineUser.class),
+                anyString());
     }
 
 }
