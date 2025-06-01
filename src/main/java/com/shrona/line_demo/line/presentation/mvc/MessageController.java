bee62226e2befebd9e56def947230186ce86c11c
shurona <khc4669@naver.com>
2025-06-01 15:46:22 +0900

    feat: 채널 정보 추가

--- src/main/java/com/shrona/line_demo/line/presentation/mvc/MessageController.java
+++ src/main/java/com/shrona/line_demo/line/presentation/mvc/MessageController.java
@@ -1,11 +1,15 @@
 package com.shrona.line_demo.line.presentation.mvc;
 
+import static com.shrona.line_demo.common.core.StaticVariable.HOME_VIEW;
 import static com.shrona.line_demo.line.presentation.form.TargetType.ALL;
 import static com.shrona.line_demo.line.presentation.form.TargetType.GROUP;
 
+import com.shrona.line_demo.common.dto.ChannelForm;
 import com.shrona.line_demo.common.dto.PagingForm;
+import com.shrona.line_demo.line.application.ChannelService;
 import com.shrona.line_demo.line.application.MessageService;
 import com.shrona.line_demo.line.application.sender.MessageSender;
+import com.shrona.line_demo.line.domain.Channel;
 import com.shrona.line_demo.line.domain.MessageLog;
 import com.shrona.line_demo.line.presentation.form.MessageListForm;
 import com.shrona.line_demo.line.presentation.form.MessageSendForm;
@@ -18,6 +22,7 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -29,27 +34,41 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
-@RequestMapping("/admin/messages")
+@RequestMapping("/admin/channels/{channelId}/messages")
 @Controller
 public class MessageController {
 
     // service
     private final MessageService messageService;
     private final GroupService groupService;
+    private final ChannelService channelService;
 
     // sender
     private final MessageSender messageSender;
 
     @GetMapping
-    public String sendMessageView(Model model) {
+    public String sendMessageView(
+        @PathVariable("channelId") Long channelId,
+        Model model) {
+
+        Optional<Channel> channelInfo = channelService.findChannelById(channelId);
+        // 채널정보가 없는 경우 그냥 홈으로 보낸다.
+        if (channelInfo.isEmpty()) {
+            return HOME_VIEW;
+        }
+
+        // 채널 정보 모델에 등록
+        registerChannelToModel(channelInfo.get(), model);
+
         // 그룹 목록 조회 및 이름 추출
-        registerGroupModel(model);
+        registerGroupModel(channelInfo.get(), model);
 
         model.addAttribute("messageForm",
             MessageSendForm.of("", LocalDateTime.now(),
@@ -63,15 +82,22 @@ public class MessageController {
      */
     @GetMapping("/list")
     public String messageListView(
+        @PathVariable("channelId") Long channelId,
         @RequestParam(value = "page", defaultValue = "0") int pageNumber,
         Model model
     ) {
         // 메시지 목록 url
         String messageListUrl = "/admin/messages/list";
 
+        Optional<Channel> channelInfo = channelService.findChannelById(channelId);
+        // 채널정보가 없는 경우 그냥 홈으로 보낸다.
+        if (channelInfo.isEmpty()) {
+            return HOME_VIEW;
+        }
+
         // 정렬을 포함해서 Pageable을 전달해서 메시지 목록을 받는다.
         Sort sort = Sort.by(Sort.Order.desc("createdAt"));
-        Page<MessageLog> messageLogList = messageService.findMessageLogList(
+        Page<MessageLog> messageLogList = messageService.findMessageLogList(channelInfo.get(),
             PageRequest.of(pageNumber, 15, sort));
 
         // groupId : 유저 갯수 Map을 조회한다.
@@ -88,6 +114,9 @@ public class MessageController {
         model.addAttribute("messages",
             messageLogList.map(m -> MessageListForm.of(m, groupUserCount)).toList());
 
+        // 채널 정보 모델에 등록
+        registerChannelToModel(channelInfo.get(), model);
+
         return "message/list";
     }
 
@@ -96,13 +125,21 @@ public class MessageController {
      */
     @PostMapping("/v1/send")
     public String sendMessage(
+        @PathVariable("channelId") Long channelId, // 채널 아이디
         Model model,
         @ModelAttribute("messageForm") MessageSendForm form,
         BindingResult bindingResult
     ) {
+        // 시간 정보를 로컬 시간 정보(utc)로 변환해준다.
         ZonedDateTime serverDateTime = form.sendDateTimeUtc()
             .withZoneSameInstant(ZoneId.systemDefault());
         LocalDateTime localDateTime = serverDateTime.toLocalDateTime();
+
+        Optional<Channel> channelInfo = channelService.findChannelById(channelId);
+        // 채널정보가 없는 경우 그냥 홈으로 보낸다.
+        if (channelInfo.isEmpty()) {
+            return HOME_VIEW;
+        }
 
         // 전송이 특정 그룹인 경우
         if (form.targetType().equals(GROUP.getType())) {
@@ -111,7 +148,9 @@ public class MessageController {
                 bindingResult.rejectValue("includeGroup", "error.non-group", "포함할 친구 그룹을 선택하세요.");
 
                 // group 목록 추가
-                registerGroupModel(model);
+                registerGroupModel(channelInfo.get(), model);
+                // 채널 정보 모델에 등록
+                registerChannelToModel(channelInfo.get(), model);
 
                 // model의 기본 데이터를 초기화 해주고 binding result 새로 매핑해준다.
                 model.addAttribute("messageForm", initMessageSendFormByForm(form));
@@ -119,7 +158,8 @@ public class MessageController {
 
                 return "message/send";
             }
-            messageService.createMessage(
+            messageService.createMessageSelectGroup(
+                channelInfo.get(),
                 1L, form.includeGroup(),
                 localDateTime,
                 form.content());
@@ -127,10 +167,10 @@ public class MessageController {
         // 전송이 전체 인 경우
         else if (form.targetType().equals(ALL.getType())) {
             messageService.createMessageAllGroup(
-                1L, form.excludeGroup(), localDateTime, form.content());
+                channelInfo.get(), 1L, form.excludeGroup(), localDateTime, form.content());
         }
 
-        return "redirect:/admin/messages/list";
+        return "redirect:/admin/channels/" + channelId + "/messages/list";
     }
 
     /**
@@ -138,9 +178,17 @@ public class MessageController {
      */
     @PostMapping("/v1/send/test")
     public ResponseEntity<?> testDeliver(
+        @PathVariable("channelId") Long channelId,
         @RequestBody MessageTestForm form
     ) {
-        if (messageSender.sendTestLineMessage(form.content())) {
+
+        Optional<Channel> channelInfo = channelService.findChannelById(channelId);
+        // 채널정보가 없는 경우 그냥 홈으로 보낸다.
+        if (channelInfo.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (messageSender.sendTestLineMessage(channelInfo.get(), form.content())) {
             return ResponseEntity.ok(Map.of("success", true));
         } else {
             return ResponseEntity.internalServerError().build();
@@ -152,8 +200,8 @@ public class MessageController {
     /**
      * Group model의 등록(메시지 보내기 화면에서 그룹 목록 보여주기)
      */
-    private void registerGroupModel(Model model) {
-        Page<Group> groupList = groupService.findGroupList(PageRequest.of(0, 1000));
+    private void registerGroupModel(Channel channel, Model model) {
+        Page<Group> groupList = groupService.findGroupList(channel, PageRequest.of(0, 1000));
 
         Map<Long, String> groupNameList = groupList.stream()
             .collect(Collectors.toMap(Group::getId, Group::getName));
@@ -168,6 +216,10 @@ public class MessageController {
     private MessageSendForm initMessageSendFormByForm(MessageSendForm form) {
         return MessageSendForm.of(form.content(), form.sendDateTime(), new ArrayList<>(),
             new ArrayList<>(), TargetType.valueOf(form.targetType()));
+    }
+
+    private void registerChannelToModel(Channel channel, Model model) {
+        model.addAttribute("channelInfo", ChannelForm.of(channel));
     }
 
 }
