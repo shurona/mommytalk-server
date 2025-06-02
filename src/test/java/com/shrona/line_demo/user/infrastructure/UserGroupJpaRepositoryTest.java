9df2d4217f588097d0fc94d1a2ecfd2ababa8e04
shurona <khc4669@naver.com>
2025-06-03 00:37:34 +0900

    test: 테스트 정보 추가

--- src/test/java/com/shrona/line_demo/user/infrastructure/UserGroupJpaRepositoryTest.java
+++ src/test/java/com/shrona/line_demo/user/infrastructure/UserGroupJpaRepositoryTest.java
@@ -3,7 +3,9 @@ package com.shrona.line_demo.user.infrastructure;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.shrona.line_demo.line.domain.Channel;
+import com.shrona.line_demo.line.domain.LineUser;
 import com.shrona.line_demo.line.infrastructure.ChannelJpaRepository;
+import com.shrona.line_demo.line.infrastructure.LineUserJpaRepository;
 import com.shrona.line_demo.user.domain.Group;
 import com.shrona.line_demo.user.domain.User;
 import com.shrona.line_demo.user.domain.UserGroup;
@@ -23,6 +25,9 @@ class UserGroupJpaRepositoryTest {
 
     @Autowired
     private UserJpaRepository userJpaRepository;
+
+    @Autowired
+    private LineUserJpaRepository lineUserJpaRepository;
 
 
     @Autowired
@@ -53,15 +58,29 @@ class UserGroupJpaRepositoryTest {
         Group group = Group.createGroup(channel, "그룹 1", "설명");
         Group group2 = Group.createGroup(channel, "그룹 2", "설명");
 
-        User user1 = userJpaRepository.save(User.createUser(new PhoneNumber("010-1234-1234")));
-        User user2 = userJpaRepository.save(User.createUser(new PhoneNumber("010-1234-1235")));
-        User user3 = userJpaRepository.save(User.createUser(new PhoneNumber("010-1234-1236")));
+        LineUser lineUser = LineUser.createLineUser("lineId");
+        LineUser lineUser2 = LineUser.createLineUser("lineId2");
+        LineUser lineUser3 = LineUser.createLineUser("lineId3");
 
+        lineUserJpaRepository.saveAll(List.of(lineUser3, lineUser2, lineUser));
+
+        User user1 = userJpaRepository.save(
+            User.createUserWithLine(new PhoneNumber("010-1234-1234"), lineUser));
+        User user2 = userJpaRepository.save(
+            User.createUserWithLine(new PhoneNumber("010-1234-1235"), lineUser2));
+        User user3 = userJpaRepository.save(
+            User.createUserWithLine(new PhoneNumber("010-1234-1236"), lineUser3));
+        // 라인 정보 없이 추가
+        User user4 = userJpaRepository.save(
+            User.createUser(new PhoneNumber("010-1234-1237")));
+
+        // 2개 추가
         group.addUserToGroup(List.of(
             UserGroup.createUserGroup(user1, group),
             UserGroup.createUserGroup(user2, group))
         );
 
+        // 1개 추가
         group2.addUserToGroup(List.of(UserGroup.createUserGroup(user3, group2)));
 
         Group groupInfo = groupJpaRepository.save(group);
