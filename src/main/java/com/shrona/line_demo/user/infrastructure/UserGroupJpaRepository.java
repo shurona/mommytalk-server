07b8d18508aae3015280c65c5aef5e4bcf2c52d3
shurona <khc4669@naver.com>
2025-06-03 00:14:03 +0900

    feat: 유저 삭제 로직 변경

--- src/main/java/com/shrona/line_demo/user/infrastructure/UserGroupJpaRepository.java
+++ src/main/java/com/shrona/line_demo/user/infrastructure/UserGroupJpaRepository.java
@@ -1,5 +1,6 @@
 package com.shrona.line_demo.user.infrastructure;
 
+import com.shrona.line_demo.user.domain.User;
 import com.shrona.line_demo.user.domain.UserGroup;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,7 +8,15 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserGroupJpaRepository extends JpaRepository<UserGroup, Long> {
 
-    @Query("SELECT gu.group.id, COUNT(gu) FROM UserGroup gu WHERE gu.group.id IN :groupIds GROUP BY gu.group.id")
+    /**
+     * 전송을 위한 user의 count 조회
+     */
+    @Query("SELECT gu.group.id, COUNT(gu) FROM UserGroup gu WHERE gu.group.id IN :groupIds AND gu.user.lineId is not null GROUP BY gu.group.id")
     List<Object[]> countByGroupIds(List<Long> groupIds);
+
+    /**
+     * user에 속한 UserGroup 조회
+     */
+    List<UserGroup> findAllByUser(User user);
 
 }
