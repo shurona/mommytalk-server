0d6fe252770e07cb428d1e508b3bb4ee739829f8
shurona <khc4669@naver.com>
2025-06-04 14:08:28 +0900

    feat: 에러 전역 핸들링

--- src/main/java/com/shrona/line_demo/common/core/GlobalControllerAdvice.java
+++ src/main/java/com/shrona/line_demo/common/core/GlobalControllerAdvice.java
@@ -3,12 +3,15 @@ package com.shrona.line_demo.common.core;
 import com.shrona.line_demo.common.dto.ErrorResponseDto;
 import com.shrona.line_demo.line.common.exception.LineException;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalControllerAdvice {
 
     /**
@@ -24,6 +27,16 @@ public class GlobalControllerAdvice {
         ErrorResponseDto response = new ErrorResponseDto(
             ex.getMessage());
         return ResponseEntity.status(ex.getCode().getStatus()).body(response);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponseDto> handle(RuntimeException ex) {
+        // 예외 로깅 등 추가 가능
+        // 에러 메시지, 코드 등 원하는 형태의 DTO로 응답
+        log.error("[전역 에러 발생] {}", ex.getMessage());
+
+        ErrorResponseDto response = new ErrorResponseDto(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
 }
