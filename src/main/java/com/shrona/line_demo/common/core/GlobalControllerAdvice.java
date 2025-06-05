fefa3614f001eadb0f73a39fe3c6e16d30680d05
shurona <khc4669@naver.com>
2025-06-05 16:50:39 +0900

    feat: 500. 405 에러 핸들링 추가

--- src/main/java/com/shrona/line_demo/common/core/GlobalControllerAdvice.java
+++ src/main/java/com/shrona/line_demo/common/core/GlobalControllerAdvice.java
@@ -4,8 +4,8 @@ import com.shrona.line_demo.common.dto.ErrorResponseDto;
 import com.shrona.line_demo.line.common.exception.LineException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -30,13 +30,21 @@ public class GlobalControllerAdvice {
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponseDto> handle(RuntimeException ex) {
+    public String handleUnExpectError(RuntimeException ex) {
         // 예외 로깅 등 추가 가능
         // 에러 메시지, 코드 등 원하는 형태의 DTO로 응답
         log.error("[전역 에러 발생] {}", ex.getMessage());
 
         ErrorResponseDto response = new ErrorResponseDto(ex.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        return "redirect:/error/500";
+    }
+
+    /**
+     * 잘못된 405 요청 시 처리
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public String handle405(HttpServletRequest req) {
+        return "redirect:/admin";
     }
 
 }
---
+++ src/main/java/com/shrona/line_demo/user/infrastructure/dao/GroupUserCount.java
@@ -0,0 +1,8 @@
+package com.shrona.line_demo.user.infrastructure.dao;
+
+public record GroupUserCount(
+    Long groupId,
+    Long ct
+) {
+
+}
