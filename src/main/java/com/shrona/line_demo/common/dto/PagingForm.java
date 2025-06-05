99bcbe0e234620e2aac37dbbc6e3277c0479c3a9
shurona <khc4669@naver.com>
2025-06-05 16:49:47 +0900

    feat: 페이징 정보 최적화

--- src/main/java/com/shrona/line_demo/common/dto/PagingForm.java
+++ src/main/java/com/shrona/line_demo/common/dto/PagingForm.java
@@ -4,20 +4,19 @@ public record PagingForm(
     int number,
     int totalPages,
     boolean hasPrevious,
-    boolean hasNext,
-    String requestUrl
+    boolean hasNext
 
 ) {
 
-    public static PagingForm of(int pageNumber, int totalPages, String requestUrl) {
+    public static PagingForm of(int pageNumber, int totalPages) {
         totalPages = (totalPages == 0) ? 1 : totalPages;
         // Front에서 페이지는 1부터 사작하므로 수정해준다.
         return new PagingForm(
             pageNumber,
             totalPages,
             pageNumber != 0,
-            (pageNumber + 1) != totalPages, // 아무 페이지도 없거나 최종페이지 확인
-            requestUrl);
+            (pageNumber + 1) != totalPages // 아무 페이지도 없거나 최종페이지 확인
+        );
     }
 
 }
