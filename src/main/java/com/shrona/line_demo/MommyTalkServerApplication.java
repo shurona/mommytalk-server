88e7e5cb665e31cd6f4e4f676e998853fd515dac
shurona <khc4669@naver.com>
2025-08-25 21:38:06 +0900

    feat: 서버 이름 변경

--- src/main/java/com/shrona/line_demo/MommyTalkServerApplication.java
+++ src/main/java/com/shrona/line_demo/MommyTalkServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LineDemoApplication {
+public class MommyTalkServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LineDemoApplication.class, args);
+        SpringApplication.run(MommyTalkServerApplication.class, args);
     }
 
     @PostConstruct
